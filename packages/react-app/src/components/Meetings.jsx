a83173262a487da5f5dad7f9667d5bbbb5b387f2
Alex <alex@MavisdeMacBook-Pro.local>
2023-01-15 00:00:48 +0800

    fix: Donation Modal wrong render

--- packages/react-app/src/components/Meetings.jsx
+++ packages/react-app/src/components/Meetings.jsx
@@ -154,12 +154,6 @@ export default function Meetings() {
       dataIndex: "site",
       key: "site",
       render: site => {
-        const marks = {
-          1: "$1",
-          5: "$5",
-          10: "$10",
-          15: "$15",
-        };
         return (
           <div style={{ display: "flex", flexDirection: "column" }}>
             <div>
@@ -177,38 +171,6 @@ export default function Meetings() {
                 </button>
               </div>
             </div>
-            <Modal title="Mint NFT，加入社区，激励创作者" visible={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-              <Row>
-                <Col span={12}>
-                  <Image width={200} height={300} src="https://i.ibb.co/q1Ysq4H/1.png" />
-                  <b>ShangHai NFT</b>
-                </Col>
-                <Col span={12}>
-                  <Image
-                    width={200}
-                    height={300}
-                    src="https://i.ibb.co/fSr67s8/Screen-Shot-2022-12-21-at-11-47-29.png"
-                  />
-                  <b>Ticket NFT</b>
-                </Col>
-              </Row>
-              <br />
-              <div>
-                选择捐赠额后Mint，可获得两枚NFT徽章，进入IdeasBazaar的城市和演讲主题频道；
-                <br />
-                <br />
-                您的捐赠额会在集会结束，退票期（72小时）过后，50%的金额转移给演讲者，10%给协议官方，40%返还给Ticket-NFT持有人。
-                <br />
-                <br />
-                因此，如果您喜欢演讲，可将Ticket-NFT转赠给演讲者。或转给所在城市的活动组织者，鼓励更多本地自组织活动发生。
-                <br />
-                <br />
-                Mint门票，积极投票或组织本地集会活动的用户将在后续获得IdeasBazaar的Token空投奖励。
-                <br />
-                <br />
-              </div>
-              <Slider marks={marks} min={1} max={15} defaultValue={10} />
-            </Modal>
           </div>
         );
       },
@@ -370,6 +332,12 @@ export default function Meetings() {
       </div>
     );
   };
+  const marks = {
+    1: "$1",
+    5: "$5",
+    10: "$10",
+    15: "$15",
+  };
   return (
     <div className="meetings">
       <Table
@@ -385,6 +353,35 @@ export default function Meetings() {
         isPrivateChannelModalOpen={isPrivateChannelModalOpen}
         setIsPrivateChannelModalOpen={setIsPrivateChannelModalOpen}
       />
+
+      <Modal title="Mint NFT，加入社区，激励创作者" visible={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+        <Row>
+          <Col span={12}>
+            <Image width={200} height={300} src="https://i.ibb.co/q1Ysq4H/1.png" />
+            <b>ShangHai NFT</b>
+          </Col>
+          <Col span={12}>
+            <Image width={200} height={300} src="https://i.ibb.co/fSr67s8/Screen-Shot-2022-12-21-at-11-47-29.png" />
+            <b>Ticket NFT</b>
+          </Col>
+        </Row>
+        <br />
+        <div>
+          选择捐赠额后Mint，可获得两枚NFT徽章，进入IdeasBazaar的城市和演讲主题频道；
+          <br />
+          <br />
+          您的捐赠额会在集会结束，退票期（72小时）过后，50%的金额转移给演讲者，10%给协议官方，40%返还给Ticket-NFT持有人。
+          <br />
+          <br />
+          因此，如果您喜欢演讲，可将Ticket-NFT转赠给演讲者。或转给所在城市的活动组织者，鼓励更多本地自组织活动发生。
+          <br />
+          <br />
+          Mint门票，积极投票或组织本地集会活动的用户将在后续获得IdeasBazaar的Token空投奖励。
+          <br />
+          <br />
+        </div>
+        <Slider marks={marks} min={1} max={15} defaultValue={10} />
+      </Modal>
     </div>
   );
 }
